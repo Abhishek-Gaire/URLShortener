86a2cb73d9a82d2dfe11b0eb872389f910bb5e33
Abhishek Gaire <118052504+Abhishek-Gaire@users.noreply.github.com>
2024-04-19 19:10:11 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const dns = require("dns");
 const app = express();
 
 // Basic Configuration
@@ -18,7 +20,43 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+let urls =[];
 
+app.post("/api/shorturl" , async(req,res) => {
+  let url = req.body.url.replace(/\/*$/, '');
+  let validUrl = url.replace(/^https:\/\/(www.)?/, '');
+  let hostname = validUrl.replace(/\.com\/.*$|\.org\/.*$/, (match) => {
+    if (match.startsWith(".com")) {
+        return ".com";
+    } else if (match.startsWith(".org")) {
+        return ".org";
+    }
+    return match; // Return the original match if it's not .com or .org
+  });  
+  dns.lookup(hostname,(err) => {
+    if(err){
+      return res.json({
+        error:"invalid url"
+      })
+    }else{
+      if(!urls.includes(url)){
+        urls.push(url);
+      }
+      return res.json({
+        original_url:url,
+        short_url:urls.indexOf(url) +1,
+      })
+    }
+  });
+})
+
+app.get("/api/shorturl/:id", (req,res) => {
+  const id = req.params.id;
+  
+  const externalURL = urls[id-1];
+  
+  res.redirect(externalURL);
+})
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
